f0ccb5341bc3a001e6c2044935b61fd4c823fe1a
Rita Bradley <rita@ritabradley.dev>
2023-06-14 13:18:08 -0700

    add Google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Bellota', 'cursive'],
+        body: ['Lato', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
