f9b7f7c700fd316179a899974c7aafe143d8f466
Rita Bradley <rita@ritabradley.dev>
2023-06-14 16:34:38 -0700

    add styles to `Intro` component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGripDots } from '@fortawesome/pro-duotone-svg-icons';
+
 export default function Intro({ name, summary }) {
   return (
     <section>
-      <h1>{name}</h1>
-      <div>
-        <p>{summary}</p>
+      <h1 className='font-display text-2xl font-bold'>{name}</h1>
+      <div className='font-body flex py-3'>
+        <FontAwesomeIcon
+          icon={faGripDots}
+          size='lg'
+          style={{ '--fa-primary-color': '#d62828', '--fa-secondary-color': '#fcbf49' }}
+        />
+        <em>
+          <p className='px-4'>{summary}</p>
+        </em>
       </div>
     </section>
   );
