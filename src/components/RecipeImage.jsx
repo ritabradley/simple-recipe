2ff4461828d2269a2c6715dc5f398d3ff7376bd0
Rita Bradley <rita@ritabradley.dev>
2023-06-14 16:38:09 -0700

    style image
    - round corners
    - add box shadow

--- src/components/RecipeImage.jsx
+++ src/components/RecipeImage.jsx
@@ -1,6 +1,7 @@
 export default function RecipeImage({ imgURL }) {
   return (
     <img
+      className='rounded-xl my-6 shadow-md'
       src={imgURL}
       alt='A slice of cheesecake with sliced strawberries on top. The slice is on a blue plate accompanied by a fork.'
     />
