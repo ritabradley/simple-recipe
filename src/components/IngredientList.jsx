ee9b3b564ee9288ba0d0e1ac9e340f237553a2ce
Rita Bradley <rita@ritabradley.dev>
2023-06-14 19:46:31 -0700

    style text

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -2,17 +2,21 @@ import Ingredient from './Ingredient';
 
 export default function IngredientList({ ingredientsList }) {
   return (
-    <>
-      <h3>Ingredients</h3>
+    <section className='mt-5'>
+      <h3 className='font-display my-3 text-xl font-bold'>Ingredients</h3>
 
       {ingredientsList.map((item, index) => (
         <div key={index}>
-          {item.name && <h4>{item.name}</h4>}
+          {item.name && (
+            <h4 className='font-display my-3 text-lg'>
+              <em>{item.name}</em>
+            </h4>
+          )}
           {item.ingredients.map((ingredient, i) => (
             <Ingredient key={i} name={ingredient.name} amount={ingredient.amount} />
           ))}
         </div>
       ))}
-    </>
+    </section>
   );
 }
