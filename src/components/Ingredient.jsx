09469e279e254ec43547627bbb2a28133b4cfd60
Rita Bradley <rita@ritabradley.dev>
2023-06-14 19:46:11 -0700

    style checkboxes

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -1,8 +1,8 @@
 export default function Ingredient({ name, amount }) {
   return (
-    <li>
-      <input type='checkbox' /> <strong className='font-black'>{amount}</strong>{' '}
-      <span dangerouslySetInnerHTML={{ __html: name }} />
+    <li className='flex items-center py-1 text-sm list-none'>
+      <input className='w-4 h-4 mr-1 accent-[#fdcf72] focus:accent-[#fcbf49]' type='checkbox' />{' '}
+      <strong className='pr-1 font-black'>{amount}</strong> <span dangerouslySetInnerHTML={{ __html: name }} />
     </li>
   );
 }
