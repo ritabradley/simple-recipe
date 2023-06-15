10bf1fcb18433eaf8bbc74cc7381881c6da1658f
Rita Bradley <rita@ritabradley.dev>
2023-06-14 20:14:26 -0700

    remove default styling from checkboxes

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -1,8 +1,31 @@
 export default function Ingredient({ name, amount }) {
   return (
     <li className='flex items-center py-1 text-sm list-none'>
-      <input className='w-4 h-4 mr-1 accent-[#fdcf72] focus:accent-[#fcbf49]' type='checkbox' />{' '}
-      <strong className='pr-1 font-black'>{amount}</strong> <span dangerouslySetInnerHTML={{ __html: name }} />
+      <input
+        className='w-4 h-4 mr-1 accent-[#fdcf72] focus:accent-[#fcbf49] appearance-none rounded relative cursor-pointer'
+        type='checkbox'
+      />
+      <strong className='pr-1 font-black'>{amount}</strong>
+      <span dangerouslySetInnerHTML={{ __html: name }} />
+      <style jsx>{`
+        input[type='checkbox']::before {
+          content: ' ';
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+          position: absolute;
+          top: -2px;
+          left: -2px;
+          background: #fff;
+          transition: all 0.3s ease;
+          border: 2px solid #fdcf72;
+        }
+        input[type='checkbox']:checked::before {
+          transform: scale(0.6);
+          opacity: 0.6;
+          background: #fdcf72;
+        }
+      `}</style>
     </li>
   );
 }
