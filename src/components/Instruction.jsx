465aee65cc7b953ba28b067562264a10170c3fd4
Rita Bradley <rita@ritabradley.dev>
2023-06-14 20:03:01 -0700

    add mobile style for instructions

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -1,7 +1,9 @@
 export default function Instruction({ step, instruction }) {
   return (
-    <li>
-      <span>{step}</span>
+    <li className='flex justify-start my-3'>
+      <span className='text-center bg-[#d62828] rounded text-white font-display font-bold text-2xl mr-3 w-9 h-9 flex items-center justify-center flex-shrink-0'>
+        {step}
+      </span>
       <p dangerouslySetInnerHTML={{ __html: instruction }} />
     </li>
   );
