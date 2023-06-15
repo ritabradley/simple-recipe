bb59724d4b8f0a8ff4270d59490022aa61e67cb0
Rita Bradley <rita@ritabradley.dev>
2023-06-14 19:09:31 -0700

    style recipe detail section for mobile

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -1,8 +1,21 @@
-export default function RecipeDetail({ title, value }) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForkKnife } from '@fortawesome/pro-duotone-svg-icons';
+
+export default function RecipeDetail({ title, value, icon, styleClass }) {
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{value}</p>
+    <div className={`py-2 mr-8 flex ${styleClass}`}>
+      <FontAwesomeIcon
+        style={
+          icon === faForkKnife
+            ? { '--fa-primary-color': '#d62828', '--fa-secondary-color': '#fcbf49' }
+            : { '--fa-primary-color': '#df5353', '--fa-secondary-color': '#fdcf72' }
+        }
+        icon={icon}
+      />
+      <div className='ml-1.5'>
+        <h3 className='text-stone-400 text-xs font-bold uppercase'>{title}</h3>
+        <p>{value}</p>
+      </div>
     </div>
   );
 }
--- src/components/RecipeDetailList.jsx
+++ src/components/RecipeDetailList.jsx
@@ -1,12 +1,37 @@
 import RecipeDetail from './RecipeDetail';
+import { faClock, faForkKnife } from '@fortawesome/pro-duotone-svg-icons';
+
+function camelCaseToNormalText(text) {
+  return text
+    .replace(/([A-Z])/g, ' $1') // insert a space before all capital letters
+    .replace(/^./, (str) => str.toUpperCase()); // capitalize the first letter
+}
 
 export default function RecipeDetailList({ recipeDetails }) {
+  const entries = Object.entries(recipeDetails);
+
   return (
-    <>
-      <h3>Recipe Details</h3>
-      {Object.entries(recipeDetails).map(([key, value]) => (
-        <RecipeDetail title={key} value={value} key={key} />
-      ))}
-    </>
+    <div className='flex flex-col'>
+      <div>
+        <RecipeDetail
+          styleClass={entries[0][0] === 'yields' ? 'text-[#d62828]' : 'text-stone-800'}
+          icon={entries[0][0] === 'yields' ? faForkKnife : faClock}
+          title={camelCaseToNormalText(entries[0][0])}
+          value={entries[0][1]}
+          key={entries[0][0]}
+        />
+      </div>
+      <div className='flex'>
+        {entries.slice(1).map(([key, value]) => (
+          <RecipeDetail
+            styleClass='text-stone-800'
+            icon={faClock}
+            title={camelCaseToNormalText(key)}
+            value={value}
+            key={key}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
